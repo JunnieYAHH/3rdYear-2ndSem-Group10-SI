5c3f1997abdb7ecb3b43b3fc4ce82f04a63a617f
JunnieYAHH <gerelitopuyos02@gmail.com>
2024-01-19 22:24:55 +0800

    Modified

--- systemintegration/src/App.js
+++ systemintegration/src/App.js
@@ -6,9 +6,21 @@ import Header from './components/layouts/Header';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-            <Header />
-          </div>
+      <Header />
+        <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        </div>
     </div>
   );
 }
